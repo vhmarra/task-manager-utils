2eb964b8cb02e1ad612a3b839bc2e668e4848887
Victor Hugo Marra Araujo <marravh@gmail.com>
2021-07-03 21:35:32 -0300

    first commit

---
+++ src/main/kotlin/br/com/victor/utilmanager/UtilManagerApplication.kt
@@ -0,0 +1,11 @@
+package br.com.victor.utilmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UtilManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<UtilManagerApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/victor/utilmanager/UtilManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.victor.utilmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UtilManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
